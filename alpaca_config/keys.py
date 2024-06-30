89789020ca8d4ace26b878a2bb3f433fe89669d4
Ivona Aleksandrova <129407856+cgshony@users.noreply.github.com>
2024-06-30 17:32:43 +0300

    Stock trading

---
+++ alpaca_config/keys.py
@@ -0,0 +1,9 @@
+config = {
+    'key_id': 'PKVK9G5A6CMEY5W8Z7ZK',
+    'secret_key': '6cbOamd1vXY4crY5ImsZFNvDiT5BkTnO2VPwxDRz',
+    'redpanda_brokers': 'localhost:9092,localhost:9093',
+    'base_url': 'https://data.alpaca.markets/v1beta1/',
+    'trade_api_base_url': 'https://paper-api.alpaca.markets/v2',
+    'slack_token': 'xoxb-7051344379409-7249668509444-I11BWj9YHqmJRDTNuoHKcPqo',
+    'slack_channel_id': 'C077BKA5AJ0',
+}
